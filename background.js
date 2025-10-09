615d3ee3e8a1f728a808a0de9ba80cc5cf7542d8
Joseph Musenge <81922031+JosephMusenge@users.noreply.github.com>
2025-10-09 00:25:38 -0500

    update background script to inject tooltip script and improve message handling

--- background.js
+++ background.js
@@ -2,8 +2,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'showTooltip') {
       chrome.scripting.executeScript({
         target: { tabId: sender.tab.id },
-        files: ['index.html'], // Injects the React app
+        files: ['static/js/tooltip.js'], // Injects the bundled react tooltip script
         injectImmediately: true,
+      }, () => {
+        chrome.tabs.sendMessage(sender.tab.id, message);
       });
     }
   });
